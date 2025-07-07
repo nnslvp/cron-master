cd1f268a6edfe111e5c504a961e9c4a027c626b7
nnslvp <nnslvp@gmail.com>
2025-07-07 23:19:26 +0000

    можешь тогда испольщовать svg для всего ?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,14 +37,11 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-        { url: '/favicon.ico', sizes: 'any' },
         { url: '/icon.svg', type: 'image/svg+xml' },
-        { url: '/favicon-16x16.png', type: 'image/png', sizes: '16x16' },
-        { url: '/favicon-32x32.png', type: 'image/png', sizes: '32x32' }
+        { url: '/favicon.ico', sizes: 'any' },
     ],
     apple: '/apple-touch-icon.png',
   },
-  manifest: '/site.webmanifest',
   themeColor: '#F0F0F5',
 };
 
@@ -60,7 +57,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Space+Grotesk:wght@400;500;700&display=swap" rel="stylesheet" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#663399" />
+        <link rel="mask-icon" href="/icon.svg" color="#663399" />
         <meta name="msapplication-TileColor" content="#663399" />
         {/* Yandex.Metrika counter */}
         <script
