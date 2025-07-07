f8e2aaa70b4e5498d4dc4a7523e5351cd6c90c39
yahorbukhta <nnslvp@gmail.com>
2025-07-07 16:45:25 +0200

    add yandex metrika

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,10 +49,35 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Space+Grotesk:wght@400;500;700&display=swap" rel="stylesheet" />
+        {/* Yandex.Metrika counter */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+              ym(103233022, "init", {
+                   clickmap:true,
+                   trackLinks:true,
+                   accurateTrackBounce:true
+              });
+            `,
+          }}
+        />
+        {/* /Yandex.Metrika counter */}
       </head>
       <body className="font-body antialiased">
         {children}
         <Toaster />
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/103233022" style={{ position: 'absolute', left: '-9999px' }} alt="" />
+          </div>
+        </noscript>
       </body>
     </html>
   );
